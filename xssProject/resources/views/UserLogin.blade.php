1a77ab728fb281cabef8600fbc36a112301ed064
Dillon Archer <132870045+Archen1898@users.noreply.github.com>
2025-04-02 09:17:36 -0400

    Boilerplate code for views

--- xssProject/resources/views/UserLogin.blade.php
+++ xssProject/resources/views/UserLogin.blade.php
@@ -1,3 +1,11 @@
-<div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <!-- Use this file to allow users to login -->
-</div>
+</body>
+</html>
--- xssProject/resources/views/ViewPosts.blade.php
+++ xssProject/resources/views/ViewPosts.blade.php
@@ -1,3 +1,11 @@
-<div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
     <!-- Use this file to allow user to view posts -->
-</div>
+</body>
+</html>
